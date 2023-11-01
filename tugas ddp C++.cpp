c6cb49f33b38baa4f2eaeeb70bca2c42fdc6060f
DivoPramudia <149493370+DivoPramudia@users.noreply.github.com>
2023-11-01 09:37:05 +0700

    Add files via upload
    tugas ddp

---
+++ tugas ddp C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 1; i <= 1; i++) {
+		cout << "----------" << endl;
+		
+		for (int j = 1; j <= 3; j++){
+			cout << "|__|__|__|";
+			cout << endl;
+		}
+	}
+	return 0;
+}
+
